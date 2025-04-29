8fe55d2e7947e5aabece49a37f1a5b734ea2fb14
suyeon <paksy0428@gmail.com>
2025-04-29 23:35:41 +0900

    refactoring users.js

--- Youtube-Demo/routes/users.js
+++ Youtube-Demo/routes/users.js
@@ -8,38 +8,35 @@ router.use(express.json()); //http 외 모듈 'json' 사용
 router.post("/login", (req, res) => {
   // email과 pw가 mariadb에 저장된 회원인지 확인
   const { email, password } = req.body;
+  let sql = `SELECT * FROM users WHERE email = ?`;
 
-  conn.query(
-    `SELECT * FROM users WHERE email = ?`,
-    email,
-    (err, results, fields) => {
-      var loginUser = results[0];
-      if (loginUser && loginUser.password == password) {
-        // login user가 있으면 && password도 같으면
-        res.status(200).json({
-          message: `${loginUser.name}님 로그인 되었습니다.`,
-        });
-      } else {
-        // login user가 없으면
-        res.status(404).json({
-          message: "이메일 또는 비밀번호가 틀립니다.",
-        });
-      }
+  conn.query(sql, email, (err, results) => {
+    var loginUser = results[0];
+    if (loginUser && loginUser.password == password) {
+      // login user가 있으면 && password도 같으면
+      res.status(200).json({
+        message: `${loginUser.name}님 로그인 되었습니다.`,
+      });
+    } else {
+      // login user가 없으면
+      res.status(404).json({
+        message: "이메일 또는 비밀번호가 틀립니다.",
+      });
     }
-  );
+  });
 });
 
 // 회원가입
 router.post("/join", (req, res) => {
   if (req.body) {
     const { email, name, password, contact } = req.body;
-    conn.query(
-      `INSERT INTO users (email, name, password, contact) VALUES (?,?,?,?) `,
-      [email, name, password, contact],
-      (err, results, fields) => {
-        res.status(201).json(results);
-      }
-    );
+
+    let sql = `INSERT INTO users (email, name, password, contact) VALUES (?,?,?,?)`;
+    let values = [email, name, password, contact];
+
+    conn.query(sql, values, (err, results, fields) => {
+      res.status(201).json(results);
+    });
   } else {
     res.status(400).json({
       message: `입력 값을 다시 확인해주세요.`,
@@ -52,33 +49,29 @@ router
   .route("/users")
   .get((req, res) => {
     let { email } = req.body;
-    conn.query(
-      `SELECT * FROM users WHERE email = ?`,
-      email,
-      (err, results, fields) => {
-        if (results.length) res.status(200).json(results);
-        else {
-          res.status(404).json({
-            message: "회원정보가 없습니다.",
-          });
-        }
+    let sql = `SELECT * FROM users WHERE email = ?`;
+
+    conn.query(sql, email, (err, results, fields) => {
+      if (results.length) res.status(200).json(results);
+      else {
+        res.status(404).json({
+          message: "회원정보가 없습니다.",
+        });
       }
-    );
+    });
   })
   .delete((req, res) => {
     let { email } = req.body;
-    conn.query(
-      `DELETE FROM users WHERE email = ?`,
-      email,
-      (err, results, fields) => {
-        if (results.affectedRows > 0) res.status(200).json(results);
-        else {
-          res.status(404).json({
-            message: "회원정보가 없습니다.",
-          });
-        }
+    let sql = `DELETE FROM users WHERE email = ?`;
+
+    conn.query(sql, email, (err, results, fields) => {
+      if (results.affectedRows > 0) res.status(200).json(results);
+      else {
+        res.status(404).json({
+          message: "회원정보가 없습니다.",
+        });
       }
-    );
+    });
   });
 
 // 모듈화
